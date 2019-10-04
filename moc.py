49bf929ec11bfa21dfaed39a68ffe9da48727d00
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-10-04 10:56:46 -0400

    Start work on orderer.

---
+++ moc.py
@@ -0,0 +1,10 @@
+def order(listIn):
+    x = 1
+    while x >= listIn.len
+        if not type(listIn[x]) in int:
+            return ValueError('This package cannot sort alphabetically yet!')
+            x += 1
+        x = 1, y = 2
+        if x > y:
+            if listIn[x] > listIn[y]:
+        elif x < y:
