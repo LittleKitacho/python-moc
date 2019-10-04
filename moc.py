532cb2931c806b8abd158239e886aa7a982b7760
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-10-04 13:50:44 -0400

    Change some things

--- moc.py
+++ moc.py
@@ -1,18 +1,31 @@
 def order(listIn):
     x = 1
-    while x >= listIn.length
+    while x >= listIn.length()
         if not type(listIn[x]) in int:
             return ValueError('This package cannot sort alphabetically yet!')
         x += 1
-    x = 1, y = 2, listOut = [], oldLength = listIn.length
-    while listOut.length <= oldLength:
-        x = 1
+    x = 1, y = 2, listOut = [], oldLength = listIn.length()
+    while listOut.length() <= oldLength:
         if listIn[x] > listIn[y]:
             x = y
-        if y = listIn.length:
+        if y = listIn.length():
             listOut.append(listIn[x])
             listIn.pop(x)
+            x = 1, y = 1
     return listOut
+####################
+from random import randint
 
 def shuffle(listIn):
-    x = 1, hold
+    if not type(listIn) in (list, tuple)
+    oldLength = listIn.length(), listOut = [], hold, take
+    while listOut.length() <= oldLength:
+        take = randint(1, listIn.length())
+        listOut.append(listIn[x])
+        listIn.pop(x)
+    return listOut
+####################
+def average(listIn):
+    x, output
+    while x <= listIn.length:
+        output += 
