594abbfc6d8c6f8badef071a99b27d8762d4bdc8
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2019-10-04 11:55:27 -0400

    Make some changes

--- moc.py
+++ moc.py
@@ -1,10 +1,9 @@
 def order(listIn):
     x = 1
-    while x >= listIn.len
+    while x >= listIn.length
         if not type(listIn[x]) in int:
             return ValueError('This package cannot sort alphabetically yet!')
-            x += 1
-        x = 1, y = 2
-        if x > y:
-            if listIn[x] > listIn[y]:
-        elif x < y:
+        x += 1
+    x = 1, y = 2, hold = 0, listOut = []
+    hold = listIn[x]
+    while listOut.length <= listIn.length
