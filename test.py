0fa93f23e814e9891011669902a756e2310f140e
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-01-09 15:42:08 +0000

    Finish test block for remove_extranious

--- test.py
+++ test.py
@@ -6,16 +6,19 @@ testList = [1, 2, 3, 4, 3, 5, 6, 9, 8, 7]
 try:
     result = moct.order(testList)
 except Exception as error:
-    raise Exception('Order returned an error')
+    raise Exception('Order returned an error while executing: '+error)
 else:
     if (result != [1, 2, 3, 3, 4, 5, 6, 7, 8, 9]):
-        raise Exception('Order returned wrong result, expected [1, 2, 3, 3, 4, 5, 6, 7, 8, 9], got '..result)
+        raise Exception('Order returned wrong result, expected [1, 2, 3, 3, 4, 5, 6, 7, 8, 9], got '+result)
     else:
-        print('Order function tested sucsessfully.')
+        print('Order function tested succsessfully.')
 
 try:
     result = moct.remove_extranious(testList)
 except Exception as error:
-    raise Exception('remove_extranious', testList, )
+    raise Exception('Remove Extranious returned an error while executing: '+error)
 else:
-    if result != [1,2,3]
+    if (result != [1, 2, 3, 4, 5, 6, 9, 8, 7]):
+        raise Exception('Remove Extranious returned wrong result, expected [1, 2, 3, 4, 5, 6, 9, 8, 7], got '+result)
+    else:
+        print('Remove Extranious tested out succsessfully')
